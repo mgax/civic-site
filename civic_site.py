9db9cd84e713e963368ff59e5d939e1cb600608f
Alex Morega <alex@grep.ro>
2011-05-19 01:42:35 +0300

    show page about a person

--- civic_site.py
+++ civic_site.py
@@ -3,16 +3,26 @@ import data
 
 app = flask.Flask(__name__)
 
+app.context_processor(data.context_processor)
+
 @app.route("/")
 def index_html():
     return flask.render_template('index.html')
 
-@app.route("/persoane")
+@app.route("/person/")
 def list_people():
     options = {
         'people': data.get_people(),
     }
     return flask.render_template('people.html', **options)
+
+@app.route("/person/<person_id>")
+def person_info(person_id):
+    person_uri = '%sperson/%s' % (data.CIVIC_URI, person_id)
+    options = {
+        'person': data.get_person(person_uri)[0],
+    }
+    return flask.render_template('person.html', **options)
 
 def main():
     app.run(debug=True)
--- data.py
+++ data.py
@@ -3,6 +3,17 @@ import sparql
 from collections import namedtuple
 
 
+CIVIC_URI = 'http://civic.grep.ro/'
+def civic_url(uri):
+    if not str(uri).startswith(CIVIC_URI):
+        #raise ValueError("unknown URI %r" % uri)
+        return "BAD_RDF_URI"
+    return '/' + str(uri)[len(CIVIC_URI):]
+
+def context_processor():
+    return {'civic_url': civic_url}
+
+
 def query(query_str):
     endpoint = "http://localhost:11746/sparql/"
     result = sparql.query(endpoint, query_str)
@@ -12,7 +23,7 @@ def query(query_str):
 
 _get_people_query = """\
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX civic_types: <http://civic.grep.ro/rdf/types/>
+PREFIX civic_types: <http://civic.grep.ro/rdftypes/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 SELECT * WHERE {
     ?person rdf:type civic_types:Person .
@@ -21,3 +32,14 @@ SELECT * WHERE {
 """
 def get_people():
     return query(_get_people_query)
+
+
+_get_person_query_tmpl = Template("""\
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+SELECT * WHERE {
+    $person foaf:name ?name .
+}
+""")
+def get_person(person_uri):
+    vars = {'person': sparql.IRI(person_uri).n3()}
+    return query(_get_person_query_tmpl.substitute(**vars))
