215a6f355aba643a5d7ff9d5e8d5b69be07168b6
Alex Morega <alex@grep.ro>
2011-05-21 00:22:24 +0300

    basic test for page describing a person

--- tests/page_tests.py
+++ tests/page_tests.py
@@ -1,11 +1,24 @@
 import unittest2 as unittest
 import lxml.html.soupparser, lxml.cssselect
+from mock import patch
 
 def tree(src):
     return lxml.html.soupparser.fromstring(src)
 
 def css(selector, node):
     return lxml.cssselect.CSSSelector(selector)(node)
+
+def csstext(selector, node):
+    return ''.join(r.text_content() for r in css(selector, node))
+
+
+class MockResponse(object):
+    def __init__(self, variable_names_str, rows):
+        self.variables = variable_names_str.split()
+        self.rows = rows
+
+    def __iter__(self):
+        return iter(self.rows)
 
 
 class PageTests(unittest.TestCase):
@@ -18,3 +31,12 @@ class PageTests(unittest.TestCase):
         page = tree(self.c.get('/').data)
         href_list = [a.attrib['href'] for a in css('a', page)]
         self.assertIn('/person/', href_list)
+
+    @patch('data.sparql')
+    def test_person_page(self, mock_sparql):
+        sparql_results = [
+            MockResponse('name', [("Gigel Jmecher",)]),
+        ]
+        mock_sparql.query.side_effect = lambda *args: sparql_results.pop()
+        page = tree(self.c.get('/person/gigel').data)
+        self.assertEqual(csstext('h1', page), "Gigel Jmecher")
