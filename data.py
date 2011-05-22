7d72e5421b2dd332a11c8fcf666cc34fe371ca15
Alex Morega <alex@grep.ro>
2011-05-22 20:52:12 +0300

    fold query back in function code

--- data.py
+++ data.py
@@ -21,17 +21,15 @@ def query(query_str):
     return [row_tuple(*sparql.unpack_row(row)) for row in result]
 
 
-_get_people_query = """\
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX civic_types: <http://civic.grep.ro/rdftypes/>
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-SELECT * WHERE {
-    ?person rdf:type civic_types:Person .
-    ?person foaf:name ?name .
-}
-"""
 def get_people():
-    return query(_get_people_query)
+    return query("""\
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX civic_types: <http://civic.grep.ro/rdftypes/>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        SELECT * WHERE {
+            ?person rdf:type civic_types:Person .
+            ?person foaf:name ?name .
+        }""")
 
 
 def get_person(person_id):
